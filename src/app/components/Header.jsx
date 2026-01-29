dc2ff7db23a79fb198d0893d5428f18826aa9363
iamlarrybob <akinbobola.lanre@gmail.com>
2026-01-28 16:55:54 -0800

    we move

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { FaSearch } from "react-icons/fa";
+function Header () {
+    return(
+        <header className="bg-slate-200 shadow-md">
+           <div className="flex items-center justify-between mx-auto max-w-6xl p-3">
+           
+                <Link href="/">
+                <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                    <span className="text-slate-500">Real</span>
+                    <span className="text-slate-700">Estate</span>
+                </h1>
+                </Link>
+
+                <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                    <input type="text"
+                    placeholder="search..."
+                    className="bg-transparent focus:outline-none w-24 sm:w-64" 
+                    />
+                    <button>
+                        <FaSearch className="text-slate-600" />
+                    </button>
+                </form>
+                <ul className="flex gap-4">
+                    <Link href="/">
+                        <li className="hidden md:inline text-slate-700 hover:underline">
+                            Home
+                        </li>
+                    </Link>
+                    <Link href="/about">
+                        <li className="hidden md:inline text-slate-700 hover:underline">
+                            About
+                        </li>
+                    </Link>
+                    <Link href="/sign-in">
+                        <li className="hidden md:inline text-slate-700 hover:underline">
+                            Sign in 
+                        </li>
+                    </Link>
+                </ul>
+
+           </div>
+        </header>
+    )
+}
+
+export default Header;
